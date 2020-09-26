5b3abc497b5062ffe55eed1a9003f5b855e0930e
Mohit Khotani <mohit.khotani@gmail.com>
2020-09-26 20:27:21 +0530

    indented block

--- src/App.js
+++ src/App.js
@@ -12,13 +12,9 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Checkbox from '@material-ui/core/Checkbox';
 
-
 function App() {
-
   const [categoryValue, setCategoryValue] = React.useState('Yes');
   const [selectValue, setSelect] = React.useState('');
-
-
 
   const handleChange = (event) => {
     setCategoryValue(event.target.value);
@@ -26,100 +22,132 @@ function App() {
   const handleSelect = (event) => {
     setSelect(event.target.value);
   };
-  const renderConditonalItem =()=>{
-    if(selectValue === true){
-      return(
+  const renderConditonalItem = () => {
+    if (selectValue === true) {
+      return (
         <FormControl>
-      <FormLabel component="legend">Hyperparameter tuning</FormLabel>
-      <RadioGroup aria-label="gender" name="Category" value={categoryValue} onChange={handleChange}>
-      <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-      <FormControlLabel value="No" control={<Radio />} label="No" />
-      </RadioGroup>
-      </FormControl>
+          <FormLabel component="legend">Hyperparameter tuning</FormLabel>
+          <RadioGroup
+            aria-label="gender"
+            name="Category"
+            value={categoryValue}
+            onChange={handleChange}
+          >
+            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+            <FormControlLabel value="No" control={<Radio />} label="No" />
+          </RadioGroup>
+        </FormControl>
       );
-    }else if(selectValue === false){
-      return(
+    } else if (selectValue === false) {
+      return (
         <Checkbox
-        defaultChecked
-        color="primary"
-        inputProps={{ 'aria-label': 'secondary checkbox' }}
-      />
+          defaultChecked
+          color="primary"
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
       );
     }
-
   };
 
   return (
     <div>
-    <h1 > Dynamic Form</h1>
-    <Grid container spacing={4}>
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <h1> Dynamic Form</h1>
+        </Grid>
+        <Grid item xs={6} className="left-div">
+          <form>
+            <h3> Input Params</h3>
+            <FormControl>
+              <div>
+                <TextField id="standard-basic" label="Index of Label Column" />
+                <InfoIcon />
+              </div>
+              <FormLabel component="legend">Automatic Cat. Column Flag</FormLabel>
+              <RadioGroup
+                aria-label="gender"
+                name="Category"
+                value={categoryValue}
+                onChange={handleChange}
+              >
+                <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+                <FormControlLabel value="No" control={<Radio />} label="No" />
+              </RadioGroup>
+              <div>
+                <TextField id="standard-basic" label="Categorical Columns" />
+                <InfoIcon />
+              </div>
+            </FormControl>
+          </form>
+        </Grid>
+        <Grid item xs={6} className="right-div">
+          <form>
+            <h3> Algo Params</h3>
+            <FormControl>
+              <FormLabel component="legend">Hyperparameter tuning</FormLabel>
+              <RadioGroup
+                aria-label="gender"
+                name="Category"
+                value={categoryValue}
+                onChange={handleChange}
+              >
+                <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+                <FormControlLabel value="No" control={<Radio />} label="No" />
+              </RadioGroup>
+              <FormLabel component="legend">K Fold Flag</FormLabel>
+              <RadioGroup
+                aria-label="gender"
+                name="Category"
+                value={categoryValue}
+                onChange={handleChange}
+              >
+                <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+                <FormControlLabel value="No" control={<Radio />} label="No" />
+              </RadioGroup>
+              <Select value={selectValue} onChange={handleSelect} displayEmpty>
+                <MenuItem value="" disabled>
+                  trainTestSplitRandomType
+                </MenuItem>
+                <MenuItem value={true}>True</MenuItem>
+                <MenuItem value={false}>False</MenuItem>
+              </Select>
 
-    <Grid item className="left-div">
-      <form>
-        <h3> Input Params</h3>
-      <FormControl>
-        <div>
-      <TextField id="standard-basic" label="Index of Label Column" />
-      <InfoIcon/>
-      </div>
-    <FormLabel component="legend">Automatic Cat. Column Flag</FormLabel>
-    <RadioGroup aria-label="gender" name="Category" value={categoryValue} onChange={handleChange}>
-        <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-        <FormControlLabel value="No" control={<Radio />} label="No" />
-        </RadioGroup>
-        <div>
-        <TextField id="standard-basic" label="Categorical Columns" />
-        <InfoIcon/>
-        </div>
-    </FormControl>
-    </form>
-    </Grid>
-    <Grid item className ="right-div">
-    <form>
-        <h3> Algo Params</h3>
-      <FormControl>
-        <FormLabel component="legend">Hyperparameter tuning</FormLabel>
-        <RadioGroup aria-label="gender" name="Category" value={categoryValue} onChange={handleChange}>
-        <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-        <FormControlLabel value="No" control={<Radio />} label="No" />
-        </RadioGroup>
-    <FormLabel component="legend">K Fold Flag</FormLabel>
-    <RadioGroup aria-label="gender" name="Category" value={categoryValue} onChange={handleChange}>
-        <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-        <FormControlLabel value="No" control={<Radio />} label="No" />
-        </RadioGroup>
-        <Select
-          value={selectValue}
-          onChange={handleSelect}
-          displayEmpty
-        >
-          <MenuItem value="" disabled>
-            trainTestSplitRandomType
-            </MenuItem>
-          <MenuItem value={true}>True</MenuItem>
-          <MenuItem value={false}>False</MenuItem>
-        </Select>
-
-        {renderConditonalItem()}
-        <div>
-        <TextField id="standard-basic" label="Training Data Percentage" className="text-field"/>
-        <InfoIcon/>
-        </div>
-        <div>
-        <TextField id="standard-basic" label="Text Data Percentage" className="text-field" />
-        <InfoIcon/>
-        </div>
-        <div>
-        <TextField id="standard-basic" label="Maximum Depth of Tree" className="text-field" />
-        <InfoIcon/>
-        </div>
-        <div>
-        <TextField id="standard-basic" label="Maximum number of bins" className="text-field" />
-        <InfoIcon/>
-        </div>
-    </FormControl>
-    </form>
-      </Grid> 
+              {renderConditonalItem()}
+              <div>
+                <TextField
+                  id="standard-basic"
+                  label="Training Data Percentage"
+                  className="text-field"
+                />
+                <InfoIcon />
+              </div>
+              <div>
+                <TextField
+                  id="standard-basic"
+                  label="Text Data Percentage"
+                  className="text-field"
+                />
+                <InfoIcon />
+              </div>
+              <div>
+                <TextField
+                  id="standard-basic"
+                  label="Maximum Depth of Tree"
+                  className="text-field"
+                />
+                <InfoIcon />
+              </div>
+              <div>
+                <TextField
+                  id="standard-basic"
+                  label="Maximum number of bins"
+                  className="text-field"
+                />
+                <InfoIcon />
+              </div>
+            </FormControl>
+          </form>
+        </Grid>
       </Grid>
     </div>
   );
